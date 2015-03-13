565937fbdb5f489acfe2a5874ad1e4f1d79fa7de
remyyounes <ryounes@gmail.com>
2015-03-13 13:15:42 -0600

    multilines

--- app/components/FractalGenerator.js
+++ app/components/FractalGenerator.js
@@ -1,26 +1,42 @@
 var React = require("react");
 var Line = require("./Line");
+var FractalIteration = require("./FractalIteration");
 var Victor = require("victor");
 
 var FractalGenerator = React.createClass({
   propTypes: {
-    width: React.PropTypes.number,
-    height: React.PropTypes.number
+    width: React.PropTypes.number.isRequired,
+    height: React.PropTypes.number.isRequired,
+    // shape: React.PropTypes.array.isRequired,
+    iterations: React.PropTypes.number.isRequired
   },
+  // I need a basic shape
+  // for each line i'll render a FractalIteration
   render() {
-    var start = new Victor(
-      0,
-      this.props.height/2
-    );
+    var shape = [
+      new Victor(
+        0,
+        this.props.height/2
+      ),
+      new Victor(
+        this.props.width/2,
+        this.props.height/3
+      ),
+      new Victor(
+        this.props.width,
+        this.props.height/2
+      )
+    ];
 
-    var end = new Victor(
-      this.props.width,
-      this.props.height/2
-    );
-
+    var iterations = [];
+    shape.reduce(function(pointA, pointB) {
+      iterations.push(<FractalIteration start={pointA} end={pointB}/>);
+      return pointB;
+    });
+    
     return (
       <svg width={this.props.width} height={this.props.height}>
-        <Line start={start} end={end}/>
+        {iterations}
       </svg>
     );
   }
